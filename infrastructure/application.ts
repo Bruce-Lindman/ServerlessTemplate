fcbced512a05fbb12c9f9b5a79021f35420ecaee
Bruce Lindman <bruce.lindman@upstart.com>
2022-02-28 14:17:14 -0500

    initial commit

---
+++ infrastructure/application.ts
@@ -0,0 +1,7 @@
+import {createLambdas} from "./upstart-cdk/create-lambdas";
+
+createLambdas('../handlers');
+
+// const application = new cdk.App();
+//
+// app.synth;
---
+++ infrastructure/upstart-cdk/create-lambdas.ts
@@ -0,0 +1,3 @@
+export const createLambdas = (rootDirectory: String) => {
+
+}
